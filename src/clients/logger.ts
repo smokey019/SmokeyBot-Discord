9f04c0bb496d0ed2bdf65fea56df9e0e2d225184
SmokeyBot <9442271+smokey019@users.noreply.github.com>
2020-06-25 06:43:05 +0000

    Update logger.ts

--- src/clients/logger.ts
+++ src/clients/logger.ts
@@ -1,6 +1,19 @@
 import log4js from 'log4js';
 import { getConfigValue } from '../config';
 
+log4js.configure({
+  appenders: {
+    out: {
+      type: 'stdout',
+      layout: {
+        type: 'pattern',
+        pattern: '%[[%d{MM/dd/yyyy - hh:mm:ss.SSS}] [%p] [%c]%] - %m%n',
+      },
+    },
+  },
+  categories: { default: { appenders: ['out'], level: 'info' } },
+});
+
 const loggers: { [category: string]: log4js.Logger } = {};
 
 const defaultLogger = '$default';
