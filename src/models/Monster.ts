81848de4190b631e00ba044099cbd3dd1f09cced
Smokey <9442271+smokey019@users.noreply.github.com>
2020-09-20 00:11:52 -0600

    new db stuff

--- src/models/Monster.ts
+++ src/models/Monster.ts
@@ -1,24 +1,24 @@
-export const MonsterTable = 'smokemon_monsters';
+export const MonsterTable = 'monsters';
 
 export interface IMonsterModel {
-  id?: number;
-  monster_id: number;
-  hp: number;
-  attack: number;
-  defense: number;
-  sp_attack: number;
-  sp_defense: number;
-  speed: number;
-  nature: string;
-  experience: number;
-  level: number;
-  /** Discord ID of the User that caught the monster. */
-  uid: string;
-  original_uid: string;
-  shiny: 0 | 1;
-  captured_at: number;
-  released?: 0 | 1;
-  favorite?: 0 | 1;
-  held_item?: number | null;
-  nickname?: string;
+	id?: number;
+	monster_id: number;
+	hp: number;
+	attack: number;
+	defense: number;
+	sp_attack: number;
+	sp_defense: number;
+	speed: number;
+	nature: string;
+	experience: number;
+	level: number;
+	/** Discord ID of the User that caught the monster. */
+	uid: string;
+	original_uid: string;
+	shiny: 0 | 1;
+	captured_at: number;
+	released?: 0 | 1;
+	favorite?: 0 | 1;
+	held_item?: number | null;
+	nickname?: string;
 }
--- src/models/MonsterUser.ts
+++ src/models/MonsterUser.ts
@@ -1,12 +1,12 @@
-export const MonsterUserTable = 'smokemon_users';
+export const MonsterUserTable = 'users';
 
 export interface IMonsterUserModel {
-  id?: number;
-  uid?: number;
-  currency?: number;
-  current_monster?: number;
-  latest_monster?: number;
-  streak?: number;
-  items?: string;
-  dex?: string;
+	id?: number;
+	uid?: number;
+	currency?: number;
+	current_monster?: number;
+	latest_monster?: number;
+	streak?: number;
+	items?: string;
+	dex?: string;
 }
