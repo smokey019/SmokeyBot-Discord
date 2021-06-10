1aa2a8c04773c3cd31a298e380944cf2179b4d92
Smokey <9442271+smokey019@users.noreply.github.com>
2021-06-09 22:38:22 -0600

    Update Monster.ts
    +added released_at

--- src/models/Monster.ts
+++ src/models/Monster.ts
@@ -17,6 +17,7 @@ export interface IMonsterModel {
 	original_uid: string;
 	shiny: 0 | 1;
 	captured_at: number;
+	released_at: number;
 	released?: 0 | 1;
 	favorite?: 0 | 1;
 	held_item?: number | null;
